91813c010dddeed02aa9d6f678e1a0ac84cd0ff3
RussiSunni <jonathandyason@hotmail.com>
2023-06-14 15:57:02 +0200

    jhk

--- js/town/scene3.js
+++ js/town/scene3.js
@@ -19,91 +19,26 @@ class Scene3 extends Phaser.Scene {
         this.bgDisplay();
 
         this.textBg = this.add.rectangle(0, 700, 540, 260, '#000000', 0.5).setOrigin(0);
-        this.narrative = this.add.text(20, 720, 'Our home.', { fontFamily: 'Arial', fill: '#ffffff', fontSize: 24 })
+        this.narrative = this.add.text(20, 720, 'Oh look, Grandma left me some money to get myself gear for the upcoming hike with Grandpa.', { fontFamily: 'Arial', fill: '#ffffff', fontSize: 24 })
 
         this.textNum = 2;
 
         this.input.on('pointerdown',
             function () {
                 if (this.textNum == 2) {
-                    this.narrative.setText('The big and bustling city.')
+                    this.narrative.setText("I'm so excited.Finally, a real adventure.")
                     this.textNum = 3;
-                    this.bgNum = 2;
                 }
-                else if (this.bgNum == 2) {
-
-                    this.bg = this.add.image(0, 0, 'elevator')
-                        .setOrigin(0)
-
-                    this.bgDisplay();
-
+                else if (this.textNum == 3) {
+                    this.narrative.setText("I'm going to go get the stuff right now.")
                     this.textNum = 4;
-                    this.textBg = this.add.rectangle(0, 700, 540, 260, '#000000', 0.5).setOrigin(0);
-                    this.narrative = this.add.text(20, 720, 'Another boring day. Nothing ever happens here.', { fontFamily: 'Arial', fill: '#ffffff', fontSize: 24, wordWrap: { width: 500, useAdvancedWrap: true } })
-
-                    this.bgNum = 3;
                 }
-                else if (this.bgNum == 3) {
-                    this.bg = this.add.image(0, 0, 'hallway')
-                        .setOrigin(0)
-                    this.bgDisplay();
-
-                    this.textBg = this.add.rectangle(0, 700, 540, 260, '#000000', 0.5).setOrigin(0);
-                    if (this.textNum == 4) {
-                        this.narrative = this.add.text(20, 720, "I wonder if Grandma's home.", { fontFamily: 'Arial', fill: '#ffffff', fontSize: 24, wordWrap: { width: 500, useAdvancedWrap: true } })
-                        this.textNum = 5;
-                    }
-                    else if (this.textNum == 5) {
-                        this.narrative = this.add.text(20, 720, "Hmm, seems like the place is empty.", { fontFamily: 'Arial', fill: '#ffffff', fontSize: 24, wordWrap: { width: 500, useAdvancedWrap: true } })
-                        this.textNum = 6;
-                        this.bgNum = 4;
-                    }
-                }
-                else if (this.bgNum == 4) {
-                    this.bg = this.add.image(0, 0, 'photo')
-                        .setOrigin(0)
-                    this.bgDisplay();
-
-                    this.textBg = this.add.rectangle(0, 700, 540, 260, '#000000', 0.5).setOrigin(0);
-                    if (this.textNum == 6) {
-                        this.narrative = this.add.text(20, 720, "I wish I could go to wild places, like Grandpa used to.", { fontFamily: 'Arial', fill: '#ffffff', fontSize: 24, wordWrap: { width: 500, useAdvancedWrap: true } })
-                        this.textNum = 7;
-                    }
-                    else if (this.textNum == 7) {
-                        this.narrative = this.add.text(20, 720, "No school, homework. No one telling me what to do.", { fontFamily: 'Arial', fill: '#ffffff', fontSize: 24, wordWrap: { width: 500, useAdvancedWrap: true } })
-                        this.textNum = 8;
-                    }
-                    else if (this.textNum == 8) {
-                        this.narrative = this.add.text(20, 720, "What an exciting life. Nothing ever happens in my life...", { fontFamily: 'Arial', fill: '#ffffff', fontSize: 24, wordWrap: { width: 500, useAdvancedWrap: true } })
-                        this.textNum = 8;
-                        this.bgNum = 5;
-                    }
-                }
-                else if (this.bgNum == 5) {
-                    this.bg = this.add.image(0, 0, 'kitchen')
-                        .setOrigin(0)
-                    this.bgDisplay();
-                    this.bgNum = 6;
-
-                    this.textBg = this.add.rectangle(0, 700, 540, 260, '#000000', 0.5).setOrigin(0);
-                    this.narrative = this.add.text(20, 720, "I'm hungry. I'll have...", { fontFamily: 'Arial', fill: '#ffffff', fontSize: 24, wordWrap: { width: 500, useAdvancedWrap: true } })
-                }
-                else if (this.bgNum == 6) {
-                    this.bg = this.add.image(0, 0, 'kitchen')
-                        .setOrigin(0)
-                    this.bgDisplay();
-                    this.bgNum = 6;
-
-                    this.textBg = this.add.rectangle(0, 700, 540, 260, '#000000', 0.5).setOrigin(0);
-                    this.narrative = this.add.text(20, 720, "I'm hungry. I'll have...", { fontFamily: 'Arial', fill: '#ffffff', fontSize: 24, wordWrap: { width: 500, useAdvancedWrap: true } })
-                    this.scene.start("Scene2");
+                else if (this.textNum == 4) {
+                    this.narrative.setText("I'll cut across the park.")
+                    this.textNum = 5;
                 }
             }, this
         );
-
-
-
-
     }
 
     bgDisplay() {
