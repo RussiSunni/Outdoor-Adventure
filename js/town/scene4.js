7f0bc7fbbfbc748676fbf9067c0cc176d452bd6e
RussiSunni <jonathandyason@hotmail.com>
2023-06-20 14:41:52 +0200

    scene4

--- js/town/scene4.js
+++ js/town/scene4.js
@@ -1,13 +1,15 @@
 export default class Scene4 extends Phaser.Scene {
     constructor() {
         super('Scene4');
-        this.textNum = 0;
+        this.narrativeNum = 0;
         this.bgNum = 0;
     }
     preload() {
         //load our images or sounds 
         this.load.image("park-entrance", "assets/Backgrounds/Town/park-entrance.png");
         this.load.image("park", "assets/Backgrounds/Town/park.png");
+        this.load.image("emily", "assets/Characters/emily.png");
+        this.load.image("zack", "assets/Characters/zack.png");
     }
 
     create() {
@@ -24,16 +26,57 @@ export default class Scene4 extends Phaser.Scene {
 
         this.input.on('pointerdown',
             function () {
+                // Park scene.
                 if (this.bgNum == 2) {
-                    this.background.setTexture('park');
-                    this.narrative.setText('')
+                    if (this.narrativeNum == 0) {
+                        this.background.setTexture('park');
+                        this.narrative.setText('')
+
+                        this.emilySprite = this.add.sprite(0, 0, 'emily').setOrigin(0.5)
+                        this.emilySprite.displayWidth = this.sys.canvas.width / 1.6
+                        this.emilySprite.displayHeight = this.sys.canvas.height
+                        this.emilySprite.setPosition(this.sys.canvas.width / 1.3, this.sys.canvas.height / 1.1);
+
+                        // Add Zack, but hide him.
+                        this.zackSprite = this.add.sprite(0, 0, 'zack').setOrigin(0.5)
+                        this.zackSprite.displayWidth = this.sys.canvas.width / 1.6
+                        this.zackSprite.displayHeight = this.sys.canvas.height
+                        this.zackSprite.setPosition(this.sys.canvas.width / 8, this.sys.canvas.height / 1.1);
+                        this.zackSprite.alpha = 0
+
+                        this.textBg = this.add.rectangle(0, this.sys.canvas.height - this.sys.canvas.height / 4, this.sys.canvas.width, this.sys.canvas.height / 4, '#000000', 0.5).setOrigin(0);
+                        this.charNameText = this.add.text(0, this.sys.canvas.height - this.sys.canvas.height / 4, 'Emily', { fontStyle: 'bold', fontFamily: 'Arial', fill: '#ffffff', fontSize: 40, wordWrap: { width: this.sys.canvas.width - 15, useAdvancedWrap: true } }).setOrigin(0, 0);
+                        this.narrative = this.add.text(0, this.sys.canvas.height - this.sys.canvas.height / 5, 'Hi!', { fontFamily: 'Arial', fill: '#ffffff', fontSize: 40, wordWrap: { width: this.sys.canvas.width - 15, useAdvancedWrap: true } }).setOrigin(0, 0);
+
+                        this.narrativeNum = 1;
+                    }
+                    else if (this.narrativeNum == 1) {
+                        // Greeting Emily.
+                        this.narrativeNum = 2;
+                        let speechOption1Graphic = this.add.graphics();
+                        speechOption1Graphic.fillStyle(0x000000, 0.5);
+                        speechOption1Graphic.fillRoundedRect(0, 0, 200, 80, 16);
+                        this.speechOption1Text = this.add.text(20, 15, "Hi Emily!", { fontFamily: 'Arial', fill: '#ffffff', fontSize: 40, wordWrap: { width: this.sys.canvas.width - 15, useAdvancedWrap: true } });
+                        this.speechOption1Container = this.add.container(this.sys.canvas.width / 2 - 100, this.sys.canvas.height / 2, [speechOption1Graphic, this.speechOption1Text]);
+                        this.speechOption1Container.setInteractive(new Phaser.Geom.Rectangle(0, 0, 1000, 100), Phaser.Geom.Rectangle.Contains);
+                        this.speechOption1Container.on('pointerdown', function () {
+                            // Show Zack.                         
+                            this.narrativeNum = 3;
+                            this.speechOption1Container.alpha = 0;
+                            this.charNameText.setText('Zack')
+                            this.narrative.setText('Hey!')
+                            this.zackSprite.alpha = 1
+                        }, this);
+                    }
+                    else if (this.narrativeNum == 3) {
+                        this.narrativeNum = 4
+                    }
+                    else if (this.narrativeNum == 4) {
+                        this.speechOption1Container.alpha = 1;
+                        this.speechOption1Text.setText('Hi Zack!')
+                    }
                 }
             }, this
         );
-    }
-
-    bgDisplay() {
-        this.bg.displayWidth = this.sys.canvas.width;
-        this.bg.displayHeight = this.sys.canvas.height;
     }
 }
