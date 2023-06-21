25e1ab576bf490d5f6f060d7beba5fafb477a24d
RussiSunni <jonathandyason@hotmail.com>
2023-06-21 15:54:13 +0200

    scene 5

--- js/town/main.js
+++ js/town/main.js
@@ -2,6 +2,7 @@ import Scene1 from './scene1.js'
 import Scene2 from './scene2.js'
 import Scene3 from './scene3.js'
 import Scene4 from './scene4.js'
+import Scene5 from './scene5.js'
 
 let config = {
     type: Phaser.AUTO,
@@ -19,6 +20,6 @@ let config = {
     },
 
 
-    scene: [Scene1, Scene2, Scene3, Scene4]
+    scene: [Scene5, Scene2, Scene3, Scene4, Scene1]
 };
 export default new Phaser.Game(config)
